8ed25135cf356c94fcd5126d0504ede748d08b82
GANAPTHIP999 <ganapathipemmani999@gmail.com>
2024-12-18 10:12:46 -0500

    function

--- mysql.sh
+++ mysql.sh
@@ -8,24 +8,16 @@ echo "Please enter DB password:"
 read -s mysql_root_password
 
 dnf install mysql-server -y &>>$LOGFILE
-#VALIDATE $? "Installing MySQL Server"
 
 systemctl enable mysqld &>>$LOGFILE
-#VALIDATE $? "Enabling MySQL Server"
+
 
 systemctl start mysqld &>>$LOGFILE
-#VALIDATE $? "Starting MySQL Server"
 
-#mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFILE
-#VALIDATE $? "Setting the root password"
-
-#Below code wull be useful for idempotatent nature
 mysql -h db.aws-9s.shop -uroot -p${mysql_root_password} -e 'show databases;' &>>$LOGFILE
 if [ $? -ne 0 ]
- then 
+then 
    mysql_secure_installation --set-root-pass ${mysql_root_password} &>>$LOGFILE
-    #VALIDATE $? "MySQL Root password Setup"
-
-  else 
+else 
     echo -e "MySQL Root password is already setup...$R SKIPPING $N"
 fi 
